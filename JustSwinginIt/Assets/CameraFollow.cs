38917ff42c83527a97041587c94c64bcc99322c1
davidhe2 <davidhe@usc.edu>
2021-09-27 15:45:17 -0700

    Added in smooth camera follow

---
+++ JustSwinginIt/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = .125f;
+
+    [SerializeField]
+    public Vector3 offset;
+
+    private Vector3 def = new Vector3(-1, -1, -1);
+
+    private void Start()
+    {
+        if(offset == def)
+            offset = new Vector3(0, 0, -20);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 destinPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, destinPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+    }
+}
