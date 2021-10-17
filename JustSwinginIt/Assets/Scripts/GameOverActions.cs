2aa401ecc6eeeaa2cc10a96b74db58a556f7b6ae
Shamanth Nataraj Dixit <52913597+dixitshamanth@users.noreply.github.com>
2021-10-16 21:00:45 -0700

    Analytics and UI screens in Level 1

---
+++ JustSwinginIt/Assets/Scripts/GameOverActions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class GameOverActions : MonoBehaviour
+{   
+    public GameOverScreen GameOverScreen;
+    private void OnTriggerEnter2D(Collider2D collision){
+    if (collision.GetComponent<Collider2D>()!=null){
+        AnalyticsResult analyticsResult = Analytics.CustomEvent(
+            "Player died",
+            new Dictionary<string,object>{
+                {"Level", 1},
+                {"Position", transform.position.x}
+            }
+        );
+        UnityEngine.Debug.Log("Death log:"+analyticsResult);
+        GameOverScreen.Setup();
+    }
+}
+}
